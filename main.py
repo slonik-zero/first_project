96f7e15a217c940c03c58c8522b51f3ceff36dbc
slonik-zero <datasciencezeroslonik@gmail.com>
2026-05-01 18:56:05 +0300

    Добавлены коды

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('Hello from main')
-print('Hello, world')
+from dotenv import load_dotenv
+import os
+
+def print_autor():
+    load_dotenv(dotenv_path='name.env')
+    autor = os.getenv("AUTOR")
+    print(f"Autor: {autor}")
+
+print_autor()
